647c4cdfc907bcd55f916a818e7dad403b3bd00b
pokemonclone <meshan314@gmail.com>
2017-06-07 11:42:07 -0700

    Add files via upload

---
+++ BattleModule.java
@@ -0,0 +1,88 @@
+package mainpackage;
+
+public class BattleModule extends Pokemon {
+	
+	/* Trying to implement the DamgeAttack/Attack methods has
+	 * its problems. Can I change the HP or stats of the
+	 * Pokemon I am attacking??? Or will I have to make
+	 * the defending pokemon an implicit parameter to make
+	 * sure that I can subtract any damage done from its HP.
+	 * This will affect all the other code as I will have to
+	 * know whether to have the attacker or the defender
+	 * as the implicit or explicit parameter.
+	 */
+		public void DamageAttack(Pokemon other) {
+			if(this.isHit(other)) {
+				if(this.isCritical(other)) {
+					//code
+				}
+				else if(this.isIneffective(other)) {
+					
+				}
+				else {
+					
+				}
+			}
+		}
+
+		public boolean isHit(Pokemon other) {
+			int z = 100 - (this.getACC() - other.getAGI());
+			if(((int)(Math.random() * 100)) <= z) {
+				return false;
+			}
+			else {
+				return true;
+			}
+		}
+		
+		// Fire beats Grass, Grass beats Water, Water beats Fire
+		// Normal type beats none
+		/* this is getting frustrated at me for using this,
+		so i may have to move it to pokemon or have battlemodule
+		extend pokemon vvvvv */
+		public boolean isCritical(Pokemon other) {
+			if((this.getType()).equals("Fire")) {
+				if(other.getType().equals("Grass")) return true;
+				if(other.getType().equals("Water")) return false;
+				if(other.getType().equals("Fire")) return false;
+				return false;
+			}
+			else if(this.getType().equals("Water")) {
+				if(other.getType().equals("Grass")) return false;
+				if(other.getType().equals("Water")) return false;
+				if(other.getType().equals("Fire")) return true;
+				return false;
+			}
+			else if(this.getType().equals("Grass")){
+				if(other.getType().equals("Grass")) return false;
+				if(other.getType().equals("Water")) return true;
+				if(other.getType().equals("Fire")) return false;
+				return false;
+			}
+			else return false;
+		}
+		//Fire is weak to Water, Grass is weak to Water, Water is
+		//weak to Grass, Normal type is weak to none
+		public boolean isIneffective(Pokemon other) {
+			if((this.getType()).equals("Fire")) {
+				if(other.getType().equals("Grass")) return false;
+				if(other.getType().equals("Water")) return true;
+				if(other.getType().equals("Fire")) return false;
+				return false;
+			}
+			else if(this.getType().equals("Water")) {
+				if(other.getType().equals("Grass")) return true;
+				if(other.getType().equals("Water")) return false;
+				if(other.getType().equals("Fire")) return false;
+				return false;
+			}
+			else if(this.getType().equals("Grass")){
+				if(other.getType().equals("Grass")) return false;
+				if(other.getType().equals("Water")) return false;
+				if(other.getType().equals("Fire")) return true;
+				return false;
+			}
+			else return false;
+		}
+		}
+}
---
+++ Pokemon.java
@@ -0,0 +1,46 @@
+package mainpackage;
+
+public class Pokemon {
+	private String Pokemon;
+    private String Type;
+    private int HP;
+    private int ATK;
+    private int AGI; //between 0-10
+    private int ACC; //between 0-100
+    
+	public Pokemon(String n, String t, int H, int AT, int AG, int AC){
+	    Pokemon = n;
+	    Type = t;
+	    HP = H;
+	    ATK = AT;
+	    AGI= AG;
+	    ACC = AC;
+	}
+	
+	public Pokemon() {
+		Pokemon = "NoNameNormal";
+		Type = "Normal";
+		HP = 100;
+		ATK = 25;
+		AGI = 5;
+		ACC = 85;
+	}
+	 public String getName(){
+	     return Pokemon;
+	 }
+	 public String getType(){
+	     return Type;
+	 }
+	 public int getHP(){
+	     return HP;
+	 }
+	 public int getATK(){
+	     return ATK;   
+	 }
+	 public int getAGI(){
+	     return AGI;
+	 }
+	 public int getACC(){
+	     return ACC;   
+	 }
+}
